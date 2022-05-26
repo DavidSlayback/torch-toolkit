f785e8f5b1e7e92fc57d3c1c7d28f8e73d9edb83
David Slayback <slayback.d@northeastern.edu>
2022-05-26 11:40:09 -0400

    Revert breaking change

--- torch_toolkit/networks/outputs.py
+++ torch_toolkit/networks/outputs.py
@@ -179,8 +179,8 @@ class DiscreteHead(ActorHead):
         Returns:
             [T?, B, n_policy?] log probabilities of sampled action
         """
-        if self.num_policies > 1 and (logits.ndim - action.ndim == 2): action = action.unsqueeze(-1)  # Add sub-policy dimension
-        return logits.gather(-1, action.unsqueeze(-1)).squeeze()
+        if self.num_policies > 1: return torch.take_along_dim(logits, action.reshape(logits.shape[:-2] + (1,1)), -1).squeeze(-1)
+        else: return logits.gather(-1, action.unsqueeze(-1)).squeeze(-1)
 
     @torch.jit.export
     def entropy(self, logits: Tensor):
